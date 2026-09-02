dfb45ad3bb9a9084a1c96f87825e113f51eacb09
Guisen Chen <2295468227@qq.com>
2026-09-02 23:11:11 +0800

    Update trimAlnSeq.py

--- trimAlnSeq.py
+++ trimAlnSeq.py
@@ -5,120 +5,261 @@ import sys
 import gzip
 import argparse
 from Bio import SeqIO
-from collections import defaultdict, Counter
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Trim fasta format alignment sequence.", add_help=False, 
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     epilog='Date:2026/06/21 Author:Guisen Chen Email:thecgs001@foxmail.com')
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
-    required.add_argument('-i', '--input', metavar='str', required=True,
-                          help='A input file of fasta format.')
-    optional.add_argument('-o', '--output', metavar='str', default='-',
-                          help='A output file of fasta format. default=None')
-    optional.add_argument('-st', '--seqtype', metavar='str', default='codon', choices=['codon', 'prot', 'nucl'],
-                          help=f'Sequence type. such as nucl, prot or codon. default=codon')
-    
-    optional.add_argument('-G', '--G', default=0, type=float, metavar='float',
-                          help=f'Gap maxinum ratio for per site. range 0-1. default=0')
-    optional.add_argument('-N', '--N', default=0, type=float, metavar='float',
-                          help=f'N maxinum ratio for per site in Nucl or Codon sequence. range 0-1. default=0')
-    optional.add_argument('-X', '--X', default=0, type=float, metavar='float',
-                          help=f'X maxinum ratio for per site in protein sequence. range 0-1. default=0')
-    
-    optional.add_argument('-ra', '--removeall', action='store_true',
-                          help=f'Remove all same columns.')
-    optional.add_argument('-h', '--help', action='help',
-                          help="Show program's help message and exit.")
-    optional.add_argument('-v', '--version', action='version', version='v2.01',
-                          help="Show program's version number and exit.")
-    
+
+def open_input(filename):
+    if filename == "-":
+        return sys.stdin
+    elif filename.endswith(".gz"):
+        return gzip.open(filename, "rt")
+    else:
+        return open(filename, "r")
+
+
+def open_output(filename):
+    if filename == "-":
+        return sys.stdout
+    elif filename.endswith(".gz"):
+        return gzip.open(filename, "wt")
+    else:
+        return open(filename, "w")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Trim fasta format alignment sequence.",
+        add_help=False,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="Date:2026/06/21 Author:Guisen Chen Email:thecgs001@foxmail.com"
+    )
+
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+
+    required.add_argument(
+        "-i", "--input",
+        metavar="str",
+        required=True,
+        help="An input alignment file in FASTA format."
+    )
+
+    optional.add_argument(
+        "-o", "--output",
+        metavar="str",
+        default="-",
+        help="Output FASTA file. default=stdout"
+    )
+
+    optional.add_argument(
+        "-st", "--seqtype",
+        metavar="str",
+        default="codon",
+        choices=["codon", "prot", "nucl"],
+        help="Sequence type: codon, prot or nucl. default=codon"
+    )
+
+    optional.add_argument(
+        "-G", "--G",
+        default=0,
+        type=float,
+        metavar="float",
+        help="Maximum gap ratio per site. range 0-1. default=0"
+    )
+
+    optional.add_argument(
+        "-N", "--N",
+        default=0,
+        type=float,
+        metavar="float",
+        help="Maximum N ratio per site for nucleotide/codon. default=0"
+    )
+
+    optional.add_argument(
+        "-X", "--X",
+        default=0,
+        type=float,
+        metavar="float",
+        help="Maximum X ratio per site for protein. default=0"
+    )
+
+    optional.add_argument(
+        "-ra", "--removeall",
+        action="store_true",
+        help="Remove invariant columns."
+    )
+
+    optional.add_argument(
+        "-h", "--help",
+        action="help",
+        help="Show help message and exit."
+    )
+
+    optional.add_argument(
+        "-v", "--version",
+        action="version",
+        version="v2.02"
+    )
+
     args = parser.parse_args()
-    
+
     infile = args.input
     outfile = args.output
-    ra = args.removeall
     st = args.seqtype
     G = args.G
     N = args.N
     X = args.X
-    
-    if infile.endswith('.gz'):
-        handle = gzip.open(infile, 'rt')
-    elif infile == "-":
-        handle = sys.stdin
-    else:
-        handle = open(infile, 'r')
-    
-    ## create index
-    indexs = []
-    aln = defaultdict(list)
-    if st!='codon':
-        Index2Seq = defaultdict(list)
-        for record in SeqIO.parse(handle, 'fasta'):
-            for n, s in enumerate(str(record.seq.upper())):
-                aln[record.id].append(s)
-                Index2Seq[n].append(s)
+    ra = args.removeall
+
+    if not (0 <= G <= 1):
+        raise ValueError("-G must be between 0 and 1")
+
+    if not (0 <= N <= 1):
+        raise ValueError("-N must be between 0 and 1")
+
+    if not (0 <= X <= 1):
+        raise ValueError("-X must be between 0 and 1")
+
+    # --------------------------------------------------
+    # Read alignment
+    # --------------------------------------------------
+
+    handle = open_input(infile)
+
+    ids = []
+    seqs = []
+
+    expected_len = None
+
+    for record in SeqIO.parse(handle, "fasta"):
+        seq = str(record.seq).upper()
+
+        if expected_len is None:
+            expected_len = len(seq)
+        elif len(seq) != expected_len:
+            raise ValueError(
+                f"Alignment length mismatch: {record.id} has length "
+                f"{len(seq)}, expected {expected_len}"
+            )
+
+        ids.append(record.id)
+        seqs.append(seq)
+
+    if handle is not sys.stdin:
         handle.close()
+
+    seqnum = len(seqs)
+
+    if seqnum == 0:
+        sys.exit("No sequences found.")
+
+    if st == "codon" and expected_len % 3 != 0:
+        raise ValueError(
+            f"Codon alignment length ({expected_len}) "
+            "is not divisible by 3."
+        )
+
+    # --------------------------------------------------
+    # Determine columns to retain
+    # --------------------------------------------------
+
+    keep = []
+
+    if st == "codon":
+
+        nsites = expected_len // 3
+
+        for site in range(nsites):
+            start = site * 3
+            end = start + 3
+
+            gap_count = 0
+            n_count = 0
+
+            first = None
+            invariant = True
+
+            for seq in seqs:
+                codon = seq[start:end]
+
+                if first is None:
+                    first = codon
+                elif codon != first:
+                    invariant = False
+
+                # gap takes priority
+                if "-" in codon:
+                    gap_count += 1
+                elif "N" in codon:
+                    n_count += 1
+
+            if gap_count / seqnum > G:
+                continue
+
+            if n_count / seqnum > N:
+                continue
+
+            if ra and invariant:
+                continue
+
+            keep.append((start, end))
+
     else:
-        Index2Seq = defaultdict(list)
-        for record in SeqIO.parse(handle, 'fasta'):
-            for n, i in enumerate(range(0, len(record.seq), 3)):
-                codon = str(record.seq.upper())[i:i+3]
-                aln[record.id].append(codon)
-                if 'N' in codon:
-                    Index2Seq[n].append("N")
-                elif '-' in codon:
-                    Index2Seq[n].append("-")
+
+        nsites = expected_len
+
+        for site in range(nsites):
+
+            gap_count = 0
+            bad_count = 0
+
+            first = seqs[0][site]
+            invariant = True
+
+            for seq in seqs:
+                base = seq[site]
+
+                if base != first:
+                    invariant = False
+
+                if base == "-":
+                    gap_count += 1
+
+                if st == "prot":
+                    if base == "X":
+                        bad_count += 1
                 else:
-                    Index2Seq[n].append(codon)
-        handle.close()
-    
-    #print(Index2Seq)
-    ## get filter site index
-    if st!='prot':
-        seqlen = len(Index2Seq.keys())
-        seqnum = len(Index2Seq[0])
-        for n in range(0, seqlen):
-            N_Ratio = Counter(Index2Seq[n]).get("N", 0) / seqnum
-            gap_Ratio = Counter(Index2Seq[n]).get("-", 0) / seqnum
-            if N < N_Ratio:
-                indexs.append(n)
-            if G < gap_Ratio:
-                indexs.append(n)
-            if (ra == True) and (len(Counter(Index2Seq[n]))) == 1:
-                indexs.append(n)
+                    if base == "N":
+                        bad_count += 1
+
+            if gap_count / seqnum > G:
+                continue
+
+            if st == "prot":
+                if bad_count / seqnum > X:
+                    continue
+            else:
+                if bad_count / seqnum > N:
+                    continue
+
+            if ra and invariant:
+                continue
+
+            keep.append(site)
+
+    # --------------------------------------------------
+    # Output
+    # --------------------------------------------------
+
+    out = open_output(outfile)
+
+    if st == "codon":
+        for name, seq in zip(ids, seqs):
+            trimmed = "".join(seq[start:end] for start, end in keep)
+            out.write(f">{name}\n{trimmed}\n")
     else:
-        seqlen = len(Index2Seq.keys())
-        seqnum = len(Index2Seq[0])
-        for n in range(0, seqlen):
-            X_Ratio = Counter(Index2Seq[n]).get("X", 0) / seqnum
-            gap_Ratio = Counter(Index2Seq[n]).get("-", 0) / seqnum
-            if X < X_Ratio:
-                indexs.append(n)
-            if G < gap_Ratio:
-                indexs.append(n)
-            if (ra == True) and (len(Counter(Index2Seq[n]))) == 1:
-                indexs.append(n)
-                
-    indexs = sorted(set(indexs))
-    #print(indexs)
-    if len(indexs) == len(list(aln.values())[0]):
-        sys.exit()
-        
-    if outfile == "-":
-        out = sys.stdout
-    elif outfile.endswith('.gz'):
-        out = gzip.open(outfile, 'wt')
-    else:
-        out = open(outfile, 'w')
-        
-    for s in aln:
-        print(">"+s, file=out)
-        seqs = ""
-        for n, codon in enumerate(aln[s]):
-            if n not in indexs:
-                seqs += codon
-        print(seqs, file=out)
-    out.close()
+        for name, seq in zip(ids, seqs):
+            trimmed = "".join(seq[i] for i in keep)
+            out.write(f">{name}\n{trimmed}\n")
+
+    if out is not sys.stdout:
+        out.close()
